55b19ddc2cb51f5125cbf62b952fe012e182ee86
Satnam Singh <satnam@raintown.org>
2014-10-20 18:18:07 -0700

    Fluent ELK demonstration

---
+++ contrib/logging/fluentd-elasticsearch/all.sh
@@ -0,0 +1,4 @@
+ kubecfg.sh -c go-ws.yml create pods
+ kubecfg.sh -c go-ws-service.yml create services
+ kubecfg.sh -c es.yml create pods
+ kubecfg.sh -c es-service.yml create services
---
+++ contrib/logging/fluentd-elasticsearch/build.sh
@@ -0,0 +1,2 @@
+docker build -t satnam6502/lambda-fluentd .
+docker push satnam6502/lambda-fluentd
